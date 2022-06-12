d5c0e5580d719589b23db464986e5a219db39f27
osoken <osoken.devel@outlook.jp>
2022-06-12 12:06:03 +0900

    test: add more tests for gen_uuid

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 from docserver import utils
 
@@ -6,4 +8,5 @@ def test_gen_uuid():
     actual = [utils.gen_uuid() for _ in range(1000)]
     assert len(set(actual)) == 1000
     assert len(set(len(d) for d in actual)) == 1
-    assert len(actual[0]) == utils.suuid_generator.encoded_length()
+    assert all(len(d) == utils.suuid_generator.encoded_length() for d in actual)
+    assert all(re.match(r"^[a-zA-Z0-9]+$", d) for d in actual)
