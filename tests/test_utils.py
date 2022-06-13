bdba34e35513ce9a0b05ab64574865a3a4ca33d5
osoken <osoken.devel@outlook.jp>
2022-06-13 23:44:19 +0900

    test: add test for gen_datetime

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import re
-
-import pytest
+from datetime import datetime, timedelta
+import freezegun
 from docserver import utils
 
 
@@ -10,3 +10,9 @@ def test_gen_uuid():
     assert len(set(len(d) for d in actual)) == 1
     assert all(len(d) == utils.suuid_generator.encoded_length() for d in actual)
     assert all(re.match(r"^[a-zA-Z0-9]+$", d) for d in actual)
+
+
+def test_gen_datetime(mocker):
+    m = mocker.patch("docserver.utils.datetime")
+    actual = utils.gen_datetime()
+    assert actual == m.utcnow.return_value
